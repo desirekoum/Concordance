0c4a65dc93f0e38418d5415d877a06eb307b1874
desir <desirekoum@yahoo.fr>
2018-10-11 22:27:11 -0400

    Created the basic structure of the project, includes data element classes. data manager classes, interfaces, and test files

---
+++ src/com/desire/ConcordanceDataManager.java
@@ -0,0 +1,17 @@
+package com.desire;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class ConcordanceDataManager implements ConcordanceDataManagerInterface {
+    @Override
+    public ArrayList<String> createConcordanceArray(String input) {
+        return null;
+    }
+
+    @Override
+    public boolean createConcordanceFile(File input, File output) throws FileNotFoundException {
+        return false;
+    }
+}
---
+++ src/com/desire/ConcordanceDataStructure.java
@@ -0,0 +1,41 @@
+package com.desire;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ConcordanceDataStructure implements ConcordanceDataStructureInterface {
+
+    public ConcordanceDataStructure(int i) {
+        super();
+    }
+    public ConcordanceDataStructure(java.lang.String test, int size){
+        super();
+
+    }
+
+
+    @Override
+    public int getTableSize() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<String> getWords(int index) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<LinkedList<Integer>> getPageNumbers(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(String word, int lineNum) {
+
+    }
+
+    @Override
+    public ArrayList<String> showAll() {
+        return null;
+    }
+}
